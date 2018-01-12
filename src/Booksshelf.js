ab4f132f8ed8c4ff01868a2305fd2e72d6164f8a
Everton Zoelner <everton.zoelner@smartgreen.net>
2018-01-12 14:04:50 -0200

    feat: Exibindo (Empty List)

--- src/Booksshelf.js
+++ src/Booksshelf.js
@@ -3,15 +3,19 @@ import Books from './Books';
 
 class Booksshelf extends Component {
   render() {
-    let { books, title, onMove} = this.props;
+    let { books, title, onMove } = this.props;
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-          {books.map((book, index) => (
-            <Books data={book} key={book.id} onMove={onMove}/>
-          ))}
+            {books.length < 1 && (
+              <h4> (Empty Shelf)</h4>
+            )}
+
+            {books.map((book) => (
+              <Books data={book} key={book.id} onMove={onMove} />
+            ))}
           </ol>
         </div>
       </div>
